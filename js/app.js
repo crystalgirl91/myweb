bd2d095a2e2283b708df70535a6f7bd2b21a2879
huangqiong <563985175@qq.com>
2015-06-06 17:11:03 +0800

    header

---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module('mainApp', ["ngRoute","ngResource","ng.controller"])
+.config(["$routeProvider",function($routeProvider){
+	$routeProvider
+	.when("/home", {
+		templateUrl: "../views/home.html",
+		controller: "HomeCtrl"
+	})
+	.when("/blog", {
+		templateUrl: "../views/blog.html",
+		controller: "BlogCtrl"
+	})
+	.when("/product", {
+		templateUrl: "../views/product.html",
+		controller: "ProductCtrl"
+	})
+	.when("/document", {
+		templateUrl: "../views/document.html",
+		controller: "DocumentCtrl"
+	})
+	.otherwise({ redirectTo: "/home" })
+}])
---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('ng.controller', [])
+.controller("HomeCtrl", ["$scope", function($scope){
+	
+}])
+.controller("BlogCtrl", ["$scope", function($scope){
+	
+}])
+.controller("ProductCtrl", ["$scope", function($scope){
+	
+}])
+.controller("DocumentCtrl", ["$scope", function($scope){
+	
+}])
