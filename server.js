c84dd16c35d8bf7e9da6124f0b222dd2fd80c94d
huangqiong <563985175@qq.com>
2015-08-26 17:45:08 +0800

    express ang mongodb

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var express = require("express");
-
-var app = express();
-app.listen(3000);
-app.post("/user/singin",function(req,res){
-	var _mess = req.body;
-	console.log(_mess);
-})
